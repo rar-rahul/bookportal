8f57e975ae7256b9a81fbb3b1029d9c89be1e7c2
rar <rahulraut430@gmail.com>
2023-02-23 01:35:19 +0530

    api blogs

--- Controller/BlogController.js
+++ Controller/BlogController.js
@@ -1,34 +1,74 @@
-const express = require('express');
-const Blog = require('../Models/Blogs');
+const express = require("express");
+const Blog = require("../Models/Blogs");
 
+exports.viewBlog = async (req, res, next) => {
+  const blogs = await Blog.find();
+  res.render("bloglist", { data: blogs });
+};
 
-exports.viewBlog = async (req,res,next) => { 
-   const blogs = await Blog.find();
-   res.render("bloglist", { data: blogs });
-}
+exports.addBlog = (req, res, next) => {
+  res.render("blog");
+};
 
-exports.addBlog = (req,res,next) => { 
-    res.render('blog');
-}
+exports.createBlog = async (req, res) => {
+  const blog = new Blog({
+    title: req.body.title,
+    description: req.body.blog_detail,
+  });
 
-exports.createBlog = async (req,res) => { 
-
-    const blog = new Blog({
-        title:req.body.title,
-        description:req.body.blog_detail
-    })
-
-    try {
+  try {
     const saveBlog = await blog.save();
-    if(saveBlog){
-        res.redirect('/view/bloglist');
+    if (saveBlog) {
+      res.redirect("/view/bloglist");
     }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+//All blogs API related function
+
+exports.getBlogs = async (req, res) => {
+  try {
+    const allBlogs = await Blog.find();
+    if (allBlogs)
+      return res.status(200).json({
+        status: "True",
+        data: allBlogs,
+      });
+  } catch (error) {
+    if (error)
+      return res.status(400).json({
+        status: "False",
+        message: "something went wrong",
+      });
+  }
+};
+
+
+exports.getSingelBlog = async (req,res) => { 
+
+    const { id } = req.params;
+   
+    try {
+        const singelBlog = await Blog.findById(id);
+        res.status(200).json({
+            singelBlog,
+            status:true
+        })
+   
     } catch (error) {
-        console.log(error);
+        res.status(401).json({
+            message:"Something went wrong",
+            status:false
+        })
     }
 
+   
 
-    
+
+
+    console.log(singelBlog)
+
+
 }
-
